4acb2e7e1d6ce7450dbe854a7341bde4917eb04e
paulgoblin <mpr75@cornell.edu>
2016-01-18 00:48:57 -0800

    add /image/thumbnail/:query route function

--- routes/image.js
+++ routes/image.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var imageUtil = require('../util/imageUtil')
 
-router.get('/:query', function(req, res, next) {
-  imageUtil.getStuff(null, function(html){
-    console.log("GOT SOME MUTHAFUCKING HTML");
-    res.json({ message: html });
+router.get('/thumbnail/:query', function(req, res, next) {
+  imageUtil.getThumbnail(req.params.query, function(url){
+    res.json({ thumbnailUrl: url });
   })
 });
 
--- util/imageUtil.js
+++ util/imageUtil.js
@@ -1,11 +1,12 @@
 var request = require('request');
+var cheerio = require('cheerio');
 
 module.exports = {
-  test: "this is a test",
-  getStuff: function(queryString, cb){
-    request.get('https://thepiratebay.se/top', function(err, res, html){
-      console.log("res!!!!!!!", html);
-      cb(html);
+  getThumbnail: function(queryString, cb){
+    request.get(`https://www.google.com/search?q=${queryString}&safe=on&tbm=isch`, function(err, res, html){
+      var $ = cheerio.load(html);
+      var thumbnail = $($('img')[0]).attr('src');
+      cb(thumbnail);
     })
   }
 }
